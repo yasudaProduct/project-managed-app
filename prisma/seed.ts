20967c46a5634a13a321df699b0e34997f124ac4
yasudaProduct <yasu.private.ct@gmail.com>
2025-02-01 20:54:30 +0900

    🌱 モックデータの詳細化と種類の拡張

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,8 @@
+import { mockData } from "../src/data/mock-data";
 import { phases } from "../src/data/phases";
 import { users } from "../src/data/users";
 import prisma from "../src/lib/prisma";
+import { ProjectStatus, TaskStatus } from "@prisma/client";
 
 async function main() {
     console.log("seed start");
@@ -40,24 +42,110 @@ async function main() {
     }
 
     // モックデータ挿入
-    // await prisma.projects.upsert({
-    //     where: { id: mockData.project.id.toString() },
-    //     update: {
-    //         name: mockData.project.name,
-    //         status: mockData.project.status as ProjectStatus,
-    //         description: mockData.project.description,
-    //         startDate: new Date(),
-    //         endDate: new Date(new Date().setDate(new Date().getDate() + 30)),
-    //     },
-    //     create: {
-    //         id: mockData.project.id.toString(),
-    //         name: mockData.project.name,
-    //         status: mockData.project.status as ProjectStatus,
-    //         description: mockData.project.description,
-    //         startDate: new Date(),
-    //         endDate: new Date(new Date().setDate(new Date().getDate() + 30)),
-    //     },
-    // })
+    await prisma.projects.upsert({
+        where: { id: mockData.project.id.toString() },
+        update: {
+            name: mockData.project.name,
+            status: mockData.project.status as ProjectStatus,
+            description: mockData.project.description,
+            startDate: new Date(),
+            endDate: new Date(new Date().setDate(new Date().getDate() + 30)),
+        },
+        create: {
+            id: mockData.project.id.toString(),
+            name: mockData.project.name,
+            status: mockData.project.status as ProjectStatus,
+            description: mockData.project.description,
+            startDate: new Date(),
+            endDate: new Date(new Date().setDate(new Date().getDate() + 30)),
+        },
+    })
+
+    for (const wbs of mockData.wbs) {
+        await prisma.wbs.upsert({
+            where: { id: wbs.id },
+            update: {
+                name: wbs.name,
+                projectId: wbs.projectId.toString(),
+            },
+            create: {
+                id: wbs.id,
+                name: wbs.name,
+                projectId: wbs.projectId.toString(),
+            },
+        })
+    }
+
+    for (const phase of mockData.wbsPhase) {
+        await prisma.wbsPhase.upsert({
+            where: { id: phase.id },
+            update: {
+                name: phase.name,
+                wbsId: phase.wbsId,
+                seq: phase.seq,
+            },
+            create: {
+                id: phase.id,
+                name: phase.name,
+                wbsId: phase.wbsId,
+                seq: phase.seq,
+            },
+        })
+    }
+
+    for (const assignee of mockData.wbsAssignee) {
+        await prisma.wbsAssignee.upsert({
+            where: { id: assignee.id },
+            update: {
+                wbsId: assignee.wbsId,
+                assigneeId: assignee.assigneeId,
+            },
+            create: {
+                wbsId: assignee.wbsId,
+                assigneeId: assignee.assigneeId,
+            },
+        })
+    }
+
+    for (const task of mockData.wbsTask) {
+        await prisma.wbsTask.upsert({
+            where: { id: task.id },
+            update: {
+                wbsId: task.wbsId,
+                phaseId: task.phaseId,
+                name: task.name,
+                assigneeId: task.assigneeId,
+                kijunStartDate: task.kijunStartDate,
+                kijunEndDate: task.kijunEndDate,
+                kijunKosu: task.kijunKosu,
+                yoteiStartDate: task.yoteiStartDate,
+                yoteiEndDate: task.yoteiEndDate,
+                yoteiKosu: task.yoteiKosu,
+                jissekiStartDate: task.jissekiStartDate,
+                jissekiEndDate: task.jissekiEndDate,
+                jissekiKosu: task.jissekiKosu,
+                status: task.status as TaskStatus,
+            },
+            create: {
+                id: task.id,
+                wbsId: task.wbsId,
+                phaseId: task.phaseId,
+                name: task.name,
+                assigneeId: task.assigneeId,
+                kijunStartDate: task.kijunStartDate,
+                kijunEndDate: task.kijunEndDate,
+                kijunKosu: task.kijunKosu,
+                yoteiStartDate: task.yoteiStartDate,
+                yoteiEndDate: task.yoteiEndDate,
+                yoteiKosu: task.yoteiKosu,
+                jissekiStartDate: task.jissekiStartDate,
+                jissekiEndDate: task.jissekiEndDate,
+                jissekiKosu: task.jissekiKosu,
+                status: task.status as TaskStatus,
+
+            },
+        })
+    }
 }
 
 main()
